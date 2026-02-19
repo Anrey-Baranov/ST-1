366a85e24455bd09ca6795352bdfaabb93439736
Anrey-Baranov <dlyneero1@gmail.com>
2026-02-19 13:54:02 +0300

    Remove build folder from git

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: A:/testirovanie/ST-1
-# Build directory: A:/testirovanie/ST-1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("test")
--- build/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: A:/testirovanie/ST-1/test
-# Build directory: A:/testirovanie/ST-1/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../_deps/googletest-build")
